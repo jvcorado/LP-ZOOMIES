db0e11ee7df8af1077c68de6ce6193f91a115e8f
beno1234 <benolopesdias@gmail.com>
2023-05-29 15:31:25 -0300

    alterar forms

---
+++ api/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "http://localhost:5000",
+});
+
+export const enviarEmail = async (formData) => {
+  const { nome, telefone, email, instagram, curriculoFile, curriculoName } =
+    formData;
+
+  const formDataToSend = new FormData();
+  formDataToSend.append("nome", nome);
+  formDataToSend.append("telefone", telefone);
+  formDataToSend.append("email", email);
+  formDataToSend.append("instagram", instagram);
+  if (curriculoFile) {
+    formDataToSend.append("curriculoFile", curriculoFile, curriculoName);
+  }
+
+  return await api.post("/send-email", formDataToSend, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
--- components/Forms/index.tsx
+++ components/Forms/index.tsx
@@ -1,70 +1,114 @@
-import { useRef, MutableRefObject } from 'react';
+import { useRef, FormEvent, ChangeEvent, useState } from "react";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { InputMask } from "primereact/inputmask";
+import { Toast } from "primereact/toast";
+import { FileUpload } from "primereact/fileupload";
+import { enviarEmail } from "../../api/api";
 
-import { Button } from "primereact/button"
-import { InputText } from "primereact/inputtext"
-import { Checkbox } from "primereact/checkbox"
-import Image from 'next/image'
-import Logo from "../../public/Logo Zoomies.png"
-import {InputMask} from "primereact/inputmask"
-import { Toast } from 'primereact/toast';
-import { FileUpload } from 'primereact/fileupload';
-
-
-export const Forms = () =>{
-
-    const toastRef = useRef<Toast>(null);
-
-    const onUpload = () => {
-        if (toastRef.current) {
-            // Lógica para exibir o toast
-            toastRef.current.show({ severity: 'info', summary: 'Success', detail: 'File Uploaded' });
-        }
-    };
-
-    return(
-        <div className="flex items-center justify-center w-full lg:p-20  lg:w-2/5">
-            <div className="surface-card p-4 shadow-2xl rounded-xl bg-white w-full">
-                <div className="text-center flex flex-col gap-4">
-                    <Image src={Logo} alt="hyper" height={50} className="m-auto" />
-                    <div className="text-900 text-3xl font-medium">Participe Agora!</div>
-                    <span className="text-600 font-medium line-height-3">Já se inscreveu?</span>
-                    <a className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Se ainda não, inscreva-se agora mesmo!</a>
-                </div>
-
-                <div className="flex flex-col gap-4">
-
-                    <label htmlFor="nome" className="block text-900 font-medium">Nome</label>
-                    <InputText id="nome" type="text" placeholder="Nome" className="w-full" />
-
-                    <label htmlFor="email" className="block text-900 font-medium">Email</label>
-                    <InputText id="email" type="text" placeholder="Email address" className="w-full" />
-
-                 
-                    <label htmlFor="fone" className="block text-900 font-medium ">Telefone</label>
-                    <InputMask mask="99-999999999" placeholder="99-999999999" />
-
-                    <div className='flex items-end justify-between gap-3'>
-                        <div className='flex flex-col flex-1 gap-3'>
-                            <label htmlFor="text" className="block text-900 font-medium ">Informe seu usuário do Instagram</label>
-                            <div className="p-inputgroup flex-1">
-                                <span className="p-inputgroup-addon">
-                                    <i className="pi pi-user"></i>
-                                </span>
-                                <InputText placeholder="@usuário" />
-                            </div>
-                        </div>
-                        <div className="card flex justify-content-center">
-                            <Toast ref={toastRef}></Toast>
-                            <FileUpload chooseLabel='Anexar ilustração' mode="basic"  name="demo[]" url="/api/upload" accept="image/*" maxFileSize={1000000} onUpload={onUpload} className='w-full flex-1' />
-                        </div>
-                    </div>
-                    <Button label="Inscrever-se" className="w-full " />
-                </div>
-            </div>
-        </div>      
-    )
+interface FormData {
+  nome: string;
+  email: string;
+  telefone: string;
+  instagram: string;
+  curriculoFile: File | null;
+  curriculoName: string;
 }
 
+export const Forms = () => {
+  const toastRef = useRef<Toast>(null);
+  const [formData, setFormData] = useState<FormData>({
+    nome: "",
+    email: "",
+    telefone: "",
+    instagram: "",
+    curriculoFile: null,
+    curriculoName: "",
+  });
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
 
+  const handleFileUpload = (e: any) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        console.log("Conteúdo do arquivo:", reader.result);
+        setFormData((prevData) => ({
+          ...prevData,
+          curriculoFile: file,
+          curriculoName: file.name,
+        }));
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await enviarEmail(formData);
+      if (toastRef.current) {
+        toastRef.current.show({
+          severity: "success",
+          summary: "Success",
+          detail: "E-mail enviado com sucesso",
+        });
+      }
+    } catch (error) {
+      console.error("Erro ao enviar e-mail:", error);
+      if (toastRef.current) {
+        toastRef.current.show({
+          severity: "error",
+          summary: "Error",
+          detail: "Erro ao enviar e-mail",
+        });
+      }
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} method="post">
+        <label htmlFor="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" onChange={handleChange} />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" onChange={handleChange} />
+
+        <label htmlFor="telefone">Telefone:</label>
+        <input
+          type="text"
+          id="telefone"
+          name="telefone"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="instagram">Instagram:</label>
+        <input
+          type="text"
+          id="instagram"
+          name="instagram"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="curriculo">Curriculo:</label>
+        <input
+          type="file"
+          id="curriculo"
+          name="curriculo"
+          onChange={handleFileUpload}
+        />
+
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+};
