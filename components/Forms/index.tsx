1b0427d2e5abd45174db272b3ce066b16ec2ab45
beno1234 <benolopesdias@gmail.com>
2023-06-06 17:57:05 -0300

    form funcionalidade

--- components/Forms/index.tsx
+++ components/Forms/index.tsx
@@ -5,7 +5,7 @@ import { InputMask } from "primereact/inputmask";
 import { Toast } from "primereact/toast";
 import { FileUpload } from "primereact/fileupload";
 import { enviarEmail } from "../../api/api";
-import Logo from "../../public/Logo Zoomies.png"
+import Logo from "../../public/Logo Zoomies.png";
 import Image from "next/image";
 
 interface FormData {
@@ -78,31 +78,43 @@ export const Forms = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit} method="post" className=" flex flex-col w-full xl:w-4/12 gap-3 p-10  shadow-2xl rounded-2xl bg-white">
-        <Image src={Logo} alt='Logo Zoomies' width={80} className="m-auto"/>
+      <form
+        onSubmit={handleSubmit}
+        method="post"
+        className=" flex flex-col w-full lg:w-4/12 gap-3 p-10  shadow-2xl rounded-2xl bg-white"
+      >
+        <Image src={Logo} alt="Logo Zoomies" width={80} className="m-auto" />
         <h1 className="text-center text-xl">Participe Agora!</h1>
         <p className="text-center">Já se inscreveu?</p>
-        <p className="text-center" style={{color:"#587EBF"}}>Se ainda não, increva-se agora mesmo!</p>
+        <p className="text-center" style={{ color: "#587EBF" }}>
+          Se ainda não, increva-se agora mesmo!
+        </p>
 
-        <label htmlFor="nome" className="text-xl">Nome:</label>
-        <input 
-            type="text" 
-            id="nome" 
-            name="nome" 
-            onChange={handleChange} 
-            className="p-2 outline-none rounded-md border-2"
+        <label htmlFor="nome" className="text-xl">
+          Nome:
+        </label>
+        <input
+          type="text"
+          id="nome"
+          name="nome"
+          onChange={handleChange}
+          className="p-2 outline-none rounded-md border-2"
         />
 
-        <label htmlFor="email" className="text-xl">Email:</label>
-        <input 
-            type="email" 
-            id="email" 
-            name="email" 
-            onChange={handleChange} 
-            className="p-2 outline-none rounded-md border-2" 
+        <label htmlFor="email" className="text-xl">
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={handleChange}
+          className="p-2 outline-none rounded-md border-2"
         />
 
-        <label htmlFor="telefone" className="text-xl">Telefone:</label>
+        <label htmlFor="telefone" className="text-xl">
+          Telefone:
+        </label>
         <input
           type="text"
           id="telefone"
@@ -111,7 +123,9 @@ export const Forms = () => {
           className="p-2 outline-none rounded-md border-2"
         />
 
-        <label htmlFor="instagram" className="text-xl">Instagram:</label>
+        <label htmlFor="instagram" className="text-xl">
+          Instagram:
+        </label>
         <input
           type="text"
           id="instagram"
@@ -120,16 +134,24 @@ export const Forms = () => {
           className="p-2 outline-none rounded-md border-2"
         />
 
-        <label htmlFor="curriculo" className="text-xl">Curriculo:</label>
+        <label htmlFor="curriculo" className="text-xl">
+          Curriculo:
+        </label>
         <input
           type="file"
           id="curriculo"
           name="curriculo"
           onChange={handleFileUpload}
-          style={{color:'#587EBF'}}
+          style={{ color: "#587EBF" }}
         />
 
-        <button type="submit" className="px-4 py-2 lg:py-4 rounded-md text-white" style={{backgroundColor:'#587EBF'}}>Insrever-se</button>
+        <button
+          type="submit"
+          className="px-4 py-2 lg:py-4 rounded-md text-white"
+          style={{ backgroundColor: "#587EBF" }}
+        >
+          Insrever-se
+        </button>
       </form>
     </>
   );
